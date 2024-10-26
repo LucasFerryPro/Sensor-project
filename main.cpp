7995979d2749821c4128dfdf92f17c0830e132c3
Lucas ferry <lucas.ferry.ecole@gmail.com>
2024-10-26 18:37:06 +0200

    project finished! (maybe)

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 #include <cmath>     // For mathematical functions
 #include <sstream>
 #include <fstream>   // For file I/O (ifstream, ofstream)
+#include <queue>     // For Dijkstra's algorithm
+#include <algorithm> // For std::reverse
+#include <set>
 
 using namespace std;
 
@@ -20,28 +23,28 @@ double euclideanDistance(const Sensor &s1, const Sensor &s2) {
 }
 
 // Function which reads data from a data file
-vector<Sensor> filedata(const string& filename) {
+vector<Sensor> filedata(const string &filename) {
     vector<Sensor> sensors; // Vector where all sensors with their parameters will be saved
     ifstream file(filename); // Predefined method used to open the file
-    string line; // Hold temporarily each line of text from file when it is read
+    string line;             // Hold temporarily each line of text from file when it is read
 
-    while (std::getline(file, line)) {
-        std::stringstream ss(line);
-        std::string token;
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string token;
         Sensor sensor;
 
         // Read and parse each value separated by commas
-        std::getline(ss, token, ',');
-        sensor.x = std::stod(token);
+        getline(ss, token, ',');
+        sensor.x = stod(token);
 
-        std::getline(ss, token, ',');
-        sensor.y = std::stod(token);
+        getline(ss, token, ',');
+        sensor.y = stod(token);
 
-        std::getline(ss, token, ',');
-        sensor.Tx = std::stod(token);
+        getline(ss, token, ',');
+        sensor.Tx = stod(token);
 
-        std::getline(ss, token, ',');
-        sensor.speed = std::stod(token);
+        getline(ss, token, ',');
+        sensor.speed = stod(token);
 
         sensors.push_back(sensor);
     }
@@ -49,7 +52,7 @@ vector<Sensor> filedata(const string& filename) {
 }
 
 // Function to create the adjacency matrix
-vector<vector<double>> AdjacencyMatrix(const vector<Sensor>& sensors) {
+vector<vector<double>> AdjacencyMatrix(const vector<Sensor> &sensors) {
     int n = sensors.size();
     vector<vector<double>> adjMatrix(n, vector<double>(n, numeric_limits<double>::infinity()));
 
@@ -67,7 +70,7 @@ vector<vector<double>> AdjacencyMatrix(const vector<Sensor>& sensors) {
 }
 
 // Function to print the adjacency matrix
-void printAdjacencyMatrix(const vector<vector<double>>& adjMatrix) {
+void printAdjacencyMatrix(const vector<vector<double>> &adjMatrix) {
     int n = adjMatrix.size();
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -81,7 +84,7 @@ void printAdjacencyMatrix(const vector<vector<double>>& adjMatrix) {
 }
 
 // Kosaraju's algorithm to find SCCs
-void dfs(int node, const vector<vector<double>>& adjMatrix, vector<bool>& visited, stack<int>& Stack) {
+void dfs(int node, const vector<vector<double>> &adjMatrix, vector<bool> &visited, stack<int> &Stack) {
     visited[node] = true;
     for (int i = 0; i < adjMatrix.size(); i++) {
         if (adjMatrix[node][i] != numeric_limits<double>::infinity() && !visited[i]) {
@@ -91,7 +94,7 @@ void dfs(int node, const vector<vector<double>>& adjMatrix, vector<bool>& visite
     Stack.push(node);
 }
 
-void reverseDfs(int node, const vector<vector<double>>& reverseAdjMatrix, vector<bool>& visited, vector<int>& component) {
+void reverseDfs(int node, const vector<vector<double>> &reverseAdjMatrix, vector<bool> &visited, vector<int> &component) {
     visited[node] = true;
     component.push_back(node);
     for (int i = 0; i < reverseAdjMatrix.size(); i++) {
@@ -102,7 +105,7 @@ void reverseDfs(int node, const vector<vector<double>>& reverseAdjMatrix, vector
 }
 
 // Function to find SCCs using Kosaraju's algorithm
-void findSCCs(const vector<vector<double>>& adjMatrix) {
+void findSCCs(const vector<vector<double>> &adjMatrix, vector<vector<int>> &clusters) {
     int n = adjMatrix.size();
     vector<bool> visited(n, false);
     stack<int> Stack;
@@ -126,7 +129,6 @@ void findSCCs(const vector<vector<double>>& adjMatrix) {
 
     // Step 3: Do DFS on the transposed graph
     fill(visited.begin(), visited.end(), false);
-    cout << "\nStrongly Connected Components (SCCs):" << endl;
     while (!Stack.empty()) {
         int node = Stack.top();
         Stack.pop();
@@ -134,6 +136,7 @@ void findSCCs(const vector<vector<double>>& adjMatrix) {
         if (!visited[node]) {
             vector<int> component;
             reverseDfs(node, reverseAdjMatrix, visited, component);
+            clusters.push_back(component);
 
             // Print the SCC
             cout << "SCC: ";
@@ -145,24 +148,248 @@ void findSCCs(const vector<vector<double>>& adjMatrix) {
     }
 }
 
-int main() {
-    // EXO 1    Get sensor datas, create and print the adjacency matrix
-    string filename = "../data_sensors10.txt";
-    vector<Sensor> sensors = filedata(filename);
-    if (sensors.empty()) {
-        cout << "No sensors found in the file." << endl;
-        return 1;
+// Tarjan's DFS-based algorithm to find articulation points
+void articulationPointsDFS(int u, int parent, const vector<vector<int>> &adjList, vector<bool> &visited,
+                           vector<int> &disc, vector<int> &low, vector<int> &parentTracker,
+                           vector<bool> &isArticulationPoint, int &time) {
+    visited[u] = true;
+    disc[u] = low[u] = ++time;
+    int children = 0;
+
+    for (int v : adjList[u]) {
+        if (!visited[v]) {
+            children++;
+            parentTracker[v] = u;
+            articulationPointsDFS(v, u, adjList, visited, disc, low, parentTracker, isArticulationPoint, time);
+
+            low[u] = min(low[u], low[v]);
+
+            if (parentTracker[u] == -1 && children > 1) {
+                isArticulationPoint[u] = true;
+            }
+
+            if (parentTracker[u] != -1 && low[v] >= disc[u]) {
+                isArticulationPoint[u] = true;
+            }
+        } else if (v != parent) {
+            low[u] = min(low[u], disc[v]);
+        }
     }
-    // Create the adjacency matrix
+}
+
+// Function to find articulation points in a cluster
+vector<int> findArticulationPointsInCluster(const vector<vector<int>> &adjList, int numSensors) {
+    vector<bool> visited(numSensors, false);
+    vector<int> disc(numSensors, -1);
+    vector<int> low(numSensors, -1);
+    vector<int> parentTracker(numSensors, -1);
+    vector<bool> isArticulationPoint(numSensors, false);
+    int time = 0;
+
+    for (int i = 0; i < numSensors; i++) {
+        if (!visited[i]) {
+            articulationPointsDFS(i, -1, adjList, visited, disc, low, parentTracker, isArticulationPoint, time);
+        }
+    }
+
+    vector<int> articulationPoints;
+    for (int i = 0; i < numSensors; i++) {
+        if (isArticulationPoint[i]) {
+            articulationPoints.push_back(i);
+        }
+    }
+    return articulationPoints;
+}
+
+// Function to create an adjacency list from the sensors data
+vector<vector<int>> createAdjacencyList(const vector<Sensor> &sensors) {
+    int n = sensors.size();
+    vector<vector<int>> adjList(n);
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (i != j) {
+                double distance = euclideanDistance(sensors[i], sensors[j]);
+                if (distance <= sensors[i].Tx && distance <= sensors[j].Tx) {
+                    adjList[i].push_back(j);
+                }
+            }
+        }
+    }
+    return adjList;
+}
+
+// Dijkstra's algorithm to find the shortest path from a source node to all other nodes
+void dijkstra(const vector<vector<double>> &adjMatrix, int src, vector<double> &distances, vector<int> &predecessors) {
+    int n = adjMatrix.size();
+    vector<bool> visited(n, false);
+    distances[src] = 0;
+
+    priority_queue<pair<double, int>, vector<pair<double, int>>, greater<pair<double, int>>> pq;
+    pq.push({0, src});
+
+    while (!pq.empty()) {
+        auto [dist, node] = pq.top();
+        pq.pop();
+
+        if (visited[node]) continue;
+        visited[node] = true;
+
+        for (int neighbor = 0; neighbor < n; ++neighbor) {
+            if (adjMatrix[node][neighbor] != numeric_limits<double>::infinity()) {
+                double newDist = distances[node] + adjMatrix[node][neighbor];
+                if (newDist < distances[neighbor]) {
+                    distances[neighbor] = newDist;
+                    predecessors[neighbor] = node;
+                    pq.push({newDist, neighbor});
+                }
+            }
+        }
+    }
+}
+
+// Function to find shortest paths in each cluster and print them
+void findShortestPathsInClusters(const vector<vector<int>> &clusters, const vector<vector<double>> &adjMatrix) {
+    for (const auto &cluster : clusters) {
+        int clusterSize = cluster.size();
+        if (clusterSize < 2) continue;
+
+        cout << "Shortest Paths in Cluster: ";
+        for (int sensor : cluster) cout << sensor << " ";
+        cout << endl;
+
+        for (int src : cluster) {
+            vector<double> distances(adjMatrix.size(), numeric_limits<double>::infinity());
+            vector<int> predecessors(adjMatrix.size(), -1);
+            dijkstra(adjMatrix, src, distances, predecessors);
+
+            for (int dest : cluster) {
+                if (src != dest) {
+                    cout << "Shortest path from " << src << " to " << dest << ": ";
+                    int u = dest;
+                    vector<int> path;
+                    while (u != -1) {
+                        path.push_back(u);
+                        u = predecessors[u];
+                    }
+                    reverse(path.begin(), path.end());
+                    for (int node : path) cout << node << " ";
+                    cout << " (Cost: " << distances[dest] << ")\n";
+                }
+            }
+            cout << endl;
+        }
+    }
+}
+
+#include <set>
+
+// Function to find the minimum number of sensors to modify for full connectivity
+void findMinimumAdjustmentsForFullConnectivity(const vector<vector<int>> &clusters, const vector<Sensor> &sensors) {
+    // Track all unique clusters
+    set<int> modifiedSensors; // To keep track of sensors we need to modify
+    int n = clusters.size();
+
+    // Determine existing connections between clusters
+    vector<vector<bool>> canConnect(n, vector<bool>(n, false));
+
+    // Iterate through each cluster and check connectivity with other clusters
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            for (int u : clusters[i]) {
+                for (int v : clusters[j]) {
+                    if (euclideanDistance(sensors[u], sensors[v]) <= sensors[u].Tx ||
+                        euclideanDistance(sensors[v], sensors[u]) <= sensors[v].Tx) {
+                        canConnect[i][j] = true;
+                        canConnect[j][i] = true; // Undirected graph
+                        break; // No need to check more pairs in this iteration
+                    }
+                }
+            }
+        }
+    }
+
+    // Identify clusters that are not directly connected
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            if (!canConnect[i][j]) {
+                // For each pair of clusters that are not connected,
+                // determine which sensor to modify in one of the clusters
+                for (int sensorInClusterA : clusters[i]) {
+                    for (int sensorInClusterB : clusters[j]) {
+                        double distance = euclideanDistance(sensors[sensorInClusterA], sensors[sensorInClusterB]);
+                        if (distance > sensors[sensorInClusterA].Tx) {
+                            // The sensor in cluster A needs to be modified
+                            modifiedSensors.insert(sensorInClusterA);
+                        }
+                        if (distance > sensors[sensorInClusterB].Tx) {
+                            // The sensor in cluster B needs to be modified
+                            modifiedSensors.insert(sensorInClusterB);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Output the results
+    cout << "Sensors to modify for full connectivity: ";
+    for (int sensor : modifiedSensors) {
+        cout << sensor << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    // Load sensors from "data_sensors10.txt"
+    vector<Sensor> sensors = filedata("../data_sensors10.txt");
+
+    // Generate the adjacency matrix
     vector<vector<double>> adjMatrix = AdjacencyMatrix(sensors);
+
     // Print the adjacency matrix
-    cout << "Adjacency Matrix (Transmission Times):" << endl;
+    cout << "Adjacency Matrix:" << endl;
     printAdjacencyMatrix(adjMatrix);
 
-    // EXO 2    Find and print SCCs
-    findSCCs(adjMatrix);
+    // Find SCCs in the network
+    vector<vector<int>> clusters;
+    findSCCs(adjMatrix, clusters);
 
-    // EXO 3    Get the shortest path between the SCCs (i think)
+    // Create the adjacency list for articulation points
+    vector<vector<int>> adjList = createAdjacencyList(sensors);
+
+    // Display articulation points in each SCC cluster
+    cout << "Articulation Points in Each Cluster:" << endl;
+    for (const auto &cluster : clusters) {
+        // Create a subgraph for the current cluster
+        vector<vector<int>> subAdjList(cluster.size());
+        for (size_t i = 0; i < cluster.size(); ++i) {
+            for (size_t j = 0; j < cluster.size(); ++j) {
+                if (i != j) {
+                    int u = cluster[i];
+                    int v = cluster[j];
+                    // Check for connections in the original adjacency list
+                    if (find(adjList[u].begin(), adjList[u].end(), v) != adjList[u].end()) {
+                        subAdjList[i].push_back(j); // add directed edge to subgraph
+                    }
+                }
+            }
+        }
+
+        vector<int> articulationPoints = findArticulationPointsInCluster(subAdjList, cluster.size());
+        cout << "Articulation Points for Cluster: ";
+        for (int point : articulationPoints) {
+            cout << point << " ";
+        }
+        cout << endl;
+    }
+
+    // Find and print shortest paths within each SCC cluster
+    cout << "Shortest Paths in Each Cluster:" << endl;
+    findShortestPathsInClusters(clusters, adjMatrix);
+
+    // Find minimum adjustments for full connectivity
+    findMinimumAdjustmentsForFullConnectivity(clusters, sensors);
 
     return 0;
 }
